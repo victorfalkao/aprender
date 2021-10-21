a0f36445bdf9f1b5b8148b24cc5ff5236896404d
victorfalkao <victorfalcao@yahoo.com.br>
2021-10-21 17:17:52 -0300

    iniciando vogais3

---
+++ vogais/vogais3.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+        <link rel="stylesheet" href="../style.css">
+        
+        <script type="text/javascript">
+            function corrigirSelecao(){
+                alert("true");
+            }
+        </script>
+        
+    </head>
+    <body>
+        <h1>SELECIONE AS IMAGENS COM OS NOMES INICIADOS PELA VOGAL DESTACADA</h1>
+        <div class="destaque">
+            <p>A</p>
+        </div>
+        <div class="div_vogais3">
+            <button type="button" onclick="corrigirSelecao()"><img src="../img2/abelha.jpg" alt="abelha"/></button>
+            <button type="button" onclick="corrigirSelecao()"><img src="../img2/apito.png" alt="apito"/></button>
+            <button type="button" onclick="corrigirSelecao()"><img src="../img2/pintinho.jpg" alt="pintinho"/></button>
+            <button type="button" onclick="corrigirSelecao()"><img src="../img2/alho.jpg" alt="alho"/></button>
+        </div>
+    </body>
+</html>
